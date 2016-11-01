0f6600e46702bb44bef70ee43f988af31fa9807a
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-11-01 19:34:11 +0500

    prefix => namespace

--- src/createAPI.js
+++ src/createAPI.js
@@ -2,13 +2,13 @@ import callAPIMethod from './callAPIMethod';
 
 import applyMiddleware from './applyMiddleware';
 
-const createAPI = (resources = {}, middleware = [], APIPrefix = '', fetchOptions = {}) =>
+const createAPI = (resources = {}, middleware = [], APINamespace = '', fetchOptions = {}) =>
 
     Object.keys(resources).reduce( (api, resourceId) => {
         api[resourceId] = Object.keys(resources[resourceId].methods).reduce( (resource, method) => {
             resource[method] = (params, methodOptions) => {
                 const apiParams = resources[resourceId].methods[method](params);
-                const bindedCallAPIMethod = callAPIMethod.bind(null, APIPrefix, fetchOptions, resources[resourceId].prefix);
+                const bindedCallAPIMethod = callAPIMethod.bind(null, APINamespace, fetchOptions, (resources[resourceId].namespace || resources[resourceId].prefix));
                 return applyMiddleware(bindedCallAPIMethod, middleware, methodOptions, apiParams, resourceId, method);
             };
             return resource;
